1d501be3a865f413cdbc397087a0fa6249fc284e
action-simon <sommergemuese@gmail.com>
2024-05-15 16:37:40 +0200

    Update page.tsx

--- app/imprint/page.tsx
+++ app/imprint/page.tsx
@@ -10,15 +10,15 @@ export default function Imprint() {
           {m.Imprint()}
         </h1>
 
-        <p className="text-xl font-bold"> {m.home_metadata_title()}</p>
+        <p className="text-xl font-bold mb-4"> {m.home_metadata_title()}</p>
 
-        <p>
+        <p className="mb-4">
           Purdey Lansink <br />
           Docent/Onderzoeker Verpleegkunde bij Saxion University of Applied Sciences <br />
           Palliatieve zorg | Minor
         </p>
 
-        <p>
+        <p className="mb-4">
           +06-1091 3357 <br />
 	  <a href="mailto:P.m.lansink@saxion.nl">P.m.lansink@saxion.nl</a>
 	</p>
