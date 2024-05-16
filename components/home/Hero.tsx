d890c37fc050e05790a3330d3c00380bb87e0f82
action-simon <sommergemuese@gmail.com>
2024-05-16 12:41:09 +0200

    Update Hero.tsx

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -39,28 +39,6 @@ const Hero = () => {
             >
               {m.home_headline_sub()}
             </motion.p>
-            <Link href="#">
-              <Button
-                size={"lg"}
-                className="mt-8 inline-flex h-12 items-center rounded-full px-6 py-4 font-semibold lg:mt-16"
-              >
-                {m.Read_more()}
-                <svg
-                  className="-mr-2 ml-8 h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </Button>
-            </Link>
           </div>
 
           <div>
@@ -69,7 +47,7 @@ const Hero = () => {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.6, ease: "easeInOut" }} // Add easing here
             >
-              <Image className="w-full" src={hero_image} alt="" />
+              <Image className="w-full rounded-lg" src={hero_image} alt="" />
             </motion.div>
           </div>
         </div>
