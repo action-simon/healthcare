15695b52265b071ead7c7314871ae1a79191bae0
action-simon <sommergemuese@gmail.com>
2024-05-16 17:52:37 +0200

    order countdown

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <Countdown />
       <Cta />
+      <Countdown />
       <Saxion />
       <KeyInformation />
       <Info />
